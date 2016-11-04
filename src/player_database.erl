554dad7cef78a9178f58701b0d0aaaf5d24d78a4
marvintau <nightvista@gmail.com>
2016-11-04 12:00:52 +0800

    added random name function

--- src/player_database.erl
+++ src/player_database.erl
@@ -2,7 +2,7 @@
 
 -author('Yue Marvin Tao').
 
--export([init_database/0, parse_form/1, login/1]).
+-export([init_database/0, parse_form/1, login/1, random_name/0]).
 
 init_database() ->
     ets:new(players, [set, public, named_table]).
@@ -14,6 +14,13 @@ parse_form(Binary) ->
     	[[_, ID], [_, Account]] -> {ID, Account};
     	NotExpected -> {malformed, NotExpected}
     end.
+
+
+random_name() ->
+    Names = [{1, "威猛的总裁"}, {2, "霸道的战士"}, {3, "机智的魔法使"}, {4, "勇敢的隐士"}, {5, "无敌的高人"}, {6, "可爱的科学家"}, {7, "呆萌的学霸"}, {8, "天然的天才"}, {9,"干练的学生会长"}, {10, "飒爽的体育部长"}],
+    erlang:display(5 > rand:uniform() * length(Names)),
+    {_, [{_, Res}|_]} = lists:splitwith(fun({ID, _Name}) -> ID < rand:uniform() * length(Names) end, Names),
+    Res.
 
 login(Binary) ->
 
